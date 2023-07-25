0c79dbe2de7247a35775c04ddd9d8b7b6440bf39
Punya Deependra Ariyadewa <deep@deep.local>
2023-07-25 16:51:54 +0530

    Added scrip

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Get the DNS server IP from /etc/resolv.conf
+dns_server=$(awk '/^nameserver/ { print $2; exit }' /etc/resolv.conf)
+
+# Generate the DNSPerf configuration file
+cat <<EOF >dnsperf.conf
+- QPS: 1000
+  Duration: 30
+  Warmup: 5
+  Threads: 10
+  Nameserver: $dns_server
+  Name: choreo.dev
+EOF
+
+# Run the DNSPerf test
+exec dnsperf -c dnsperf.conf
+
