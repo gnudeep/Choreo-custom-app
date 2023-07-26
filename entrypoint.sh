34ce471d8c0bb5e1e1f8a7f533ac888da6e4053a
Punya Deependra Ariyadewa <deep@deep.local>
2023-07-26 12:14:32 +0530

    remove w

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # Get the DNS server IP from /etc/resolv.conf
 dns_server=$(awk '/^nameserver/ { print $2; exit }' /etc/resolv.conf)
 
-exec dnsperf -s "$dns_server" -d 10 -l 30 -w 5 -c 10 choreo.dev
+exec dnsperf -s "$dns_server" -d 10 -l 30 -c 10 choreo.dev
 
